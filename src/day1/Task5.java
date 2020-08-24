b6187c172dc19a90393cd379b483d176290d603e
George Bezmenov <g.verman2014@yandex.ru>
2020-08-24 14:20:11 +0900

    Add day1 solution

---
+++ src/day1/Task5.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class Task5 {
+    public static void main(String[] args) {
+        int age;
+        for (age = 29; age < 100; age += 5) {
+            System.out.println("Мой возраст " + age);
+        }
+    }
+}
