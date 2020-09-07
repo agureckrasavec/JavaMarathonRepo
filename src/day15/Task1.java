d1e4d40c8e718278e3f3b7d8dd9bd551f4cf4d34
George Bezmenov <g.verman2014@yandex.ru>
2020-09-07 23:14:32 +0900

    Add day15 solution fix

--- src/day15/Task1.java
+++ src/day15/Task1.java
@@ -4,12 +4,11 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Task1 {
     public static void main(String[] args) {
         try {
             FileInputStream fileInputStream = new FileInputStream("shoes.csv");
-            FileWriter fileWriter = new FileWriter("day15");
+            FileWriter fileWriter = new FileWriter("day15.txt");
             BufferedReader rd = new BufferedReader(new InputStreamReader(fileInputStream));
             List<String> strings = new ArrayList<>();
 
@@ -18,16 +17,16 @@ public class Task1 {
             }
             fileInputStream.close();
             rd.close();
-            for (String s : strings) {
-                if (s.contains(";0")) {
-                    fileWriter.write(s + "\n");
-                }
+            for (int i = 0; i < strings.size(); i++) {
+                String[] line = strings.get(i).split(";");
+                if (line[line.length - 1].equals("0"))
+                    fileWriter.write(strings.get(i) + "\n");
             }
             fileWriter.close();
         } catch (FileNotFoundException e) {
             System.out.println("Файл не найден");
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Некорректный входной файл");
         }
 
     }
