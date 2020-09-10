fdc7242fbf89aced7986bd937d0b6e98569496d6
George Bezmenov <g.verman2014@yandex.ru>
2020-09-11 08:10:37 +0900

    Add day18 soltions fix

--- src/day18/Task2.java
+++ src/day18/Task2.java
@@ -2,11 +2,11 @@ package day18;
 
 public class Task2 {
     public static void main(String[] args) {
-        System.out.println(sevenQuantity(134377777));
+        System.out.println(sevenQuantity(1217234877));
     }
 
     public static int sevenQuantity(int n) {
-      if (n / 10 == 0) {
+      if (n == 0) {
           return 0;
       }
       return ((n % 10 == 7) ? 1 : 0) + sevenQuantity(n / 10);
